76a6783645593c895e54efd5211feaada3d74a88
Mateus Gonçalves melo <mateusgmeloo@gmail.com>
2024-05-16 00:44:23 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import{ View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
 
-export default function App() {
-  return (
+export default function App(){
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+      source={require("./novalogo.png")}
+      style={styles.logo}
+      />
+      
+      <TouchableOpacity style={styles.Login}>
+          <Text style={styles.TextLogin} >LOGIN</Text>
+       </TouchableOpacity>
+
+      <TouchableOpacity style={styles.Newaccout}>
+        <Text style={styles.Textaccount}>CRIAR CONTA</Text>
+      </TouchableOpacity>
+     
     </View>
-  );
+    
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
+const styles= StyleSheet.create({
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff0ff",
+    justifyContent: "center",
+    alignItems: "center",
+    },
+
+
+  logo:{
+    marginBottom: 100,
+    
+
   },
-});
+  Login: {
+    marginBottom:60,
+    width: 250,
+    height: 50, 
+    backgroundColor: "#00366B",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 15
+   },
+
+
+   TextLogin:{
+    color: "#fff",
+    fontFamily: "Sarala"
+   },
+
+
+   Newaccout:{
+    marginBottom: 60,
+    width: 250,
+    height: 50, 
+    backgroundColor: "#00366B",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 15,
+   
+   },
+
+
+   Textaccount:{
+    color: "#fff",
+  }
+})
