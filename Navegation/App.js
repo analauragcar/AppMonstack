34cbe34df95e9bc4946dbd0613833057ef49de95
analauragcar <148863673+analauragcar@users.noreply.github.com>
2024-06-03 20:11:07 -0300

    vinculei os códigos

--- Navegation/App.js
+++ Navegation/App.js
@@ -5,6 +5,8 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import App from '../Screens/screen1'
 import Login from '../Screens/screen2'
+import Cadastro from '../Screens/screen3'
+import Bateria from '../Screens/screen4'
 
 const Stack = createStackNavigator();
 
@@ -14,6 +16,8 @@ export default function App(){
             <Stack.Navigator initialRouteName="App">
                 <Stack.Screen name='login' component={Login}/> 
                 <Stack.Screen name='App' component={App}/>
+                <Stack.Screen name='Cadastro' component={Cadastro}/>
+                <Stack.Screen name='Bateria' component={Bateria}/>
             </Stack.Navigator>
         </NavigationContainer>
             );
