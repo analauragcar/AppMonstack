da9a3e15eeaf37ae4d818f10ca3303712ba3a5a5
analauragcar <148863673+analauragcar@users.noreply.github.com>
2024-06-03 17:33:02 -0300

    movi de pasta

--- Navegation/App.js
+++ Navegation/App.js
@@ -3,18 +3,17 @@ import { Link } from 'react-router-dom';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-import App from './App'
-import login from './Telas'
+import App from '../Screens/screen1'
+import Login from '../Screens/screen2'
 
 const Stack = createStackNavigator();
 
 export default function App(){
     return(
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="login">
-                <Stack.Screen name='login' component={login}/> 
+            <Stack.Navigator initialRouteName="App">
+                <Stack.Screen name='login' component={Login}/> 
                 <Stack.Screen name='App' component={App}/>
-               
             </Stack.Navigator>
         </NavigationContainer>
             );
